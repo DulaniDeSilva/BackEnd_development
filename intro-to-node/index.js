35ab376373a4f486f254f2a97389b38905c35d34
DulaniDeSilva <addsdesilva8@gmail.com>
2023-10-03 23:33:42 +0530

    Backend intro

---
+++ intro-to-node/index.js
@@ -0,0 +1,17 @@
+// file system module
+// var creates a variable cnst stands for constant
+// const a=5;
+// const pi = 3.14;
+
+//fs - file system; inernal nodejs module
+// const fs = require("fs");
+
+//creating a copy
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+//external nodejs module
+var superheroes = require("superheroes");
+var mysuper = superheroes.random();
+console.log(mysuper);
+
+
