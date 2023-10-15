78e1c30cedb84cf3e4b2828df0f9a35056e89965
DulaniDeSilva <addsdesilva8@gmail.com>
2023-10-15 09:02:36 +0530

    Add backend technology express, ejx

--- API/Newsletter-Signup/app.js
+++ API/Newsletter-Signup/app.js
@@ -3,11 +3,20 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", function(req, res){
-    res.send("Hello, world!");
+    res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+   
+    var email = requ.body.email;
+    console.log(email);
+
+})
 
 
 app.listen(3000, function(){
--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -1,16 +1,35 @@
 const express = require("express");
-const bodyParser = require("bodt-parser");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+var items = [ "Buy food", "Sell"];
+
 app.get("/", function(req, res){
-//    
-var tody = new Date();
-if(today.getDay() == 6 || WebTransportBidirectionalStream.getDay() == o{
-    
-})
+  
+
+    let day = date();
+   res.render("list",{
+    kindofDay:day,
+    newListItems: items
+   });
 
 
+});
+
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 
